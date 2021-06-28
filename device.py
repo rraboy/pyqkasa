604fe39866f56beab881bd90c43f7ed0614bfe43
Randall Raboy <contact@randallraboy.com>
2021-06-28 00:56:31 -0400

    added logging and refactor plugdevice to publish additional values

--- device.py
+++ device.py
@@ -1,7 +1,11 @@
 
 import asyncio
+import logging
+import json 
 
 from kasa import SmartPlug
+
+log = logging.getLogger(f"main.{__name__}")
 
 class PlugDevice:
     def __init__(self, config, mqtt):
@@ -13,22 +17,31 @@ class PlugDevice:
         asyncio.run(self._update())
 
     def subscribe(self, mqtt):
-        mqtt.subscribe(f"{self.device_id}/states/switch_on")
+        mqtt.subscribe(f"{self.device_id}/command/+")
 
-    def on_message(self, msg):
+    def publish_val(self, name, value):
+        topic = f"{self.device_id}/values/{name}"
+        self.mqtt.publish(topic, str(value), 1)
+        log.debug('Topic: %s, Value: %s', topic, value)
+
+    def on_command(self, cmd, msg):
         value = msg.strip().lower()
-        if value == "true" or value == "yes":
-            print('Turning on ' + self.device_id)
-            asyncio.run(self.plug.turn_on())
-            self.update()
-        elif value == "false" or value == "no":
-            print('Turning off ' + self.device_id)
-            asyncio.run(self.plug.turn_off())
+
+        if cmd == 'power_state':
+            if value == "true" or value == "yes":
+                asyncio.run(self.plug.turn_on())
+                self.update()
+                log.debug(f"Turning on {self.device_id}")
+            elif value == "false" or value == "no":
+                asyncio.run(self.plug.turn_off())
+                self.update()
+                log.debug(f"Turning off {self.device_id}")
+        elif cmd == 'sync':
             self.update()
 
     async def _update(self):
         await self.plug.update()
-        print(self.plug.alias)
-        print(self.plug.is_on)
-
-        self.mqtt.publish(f"{self.device_id}/values/is_on", str(self.plug.is_on).lower(), 1)
+        sysinfo = await self.plug.get_sys_info()
+        self.publish_val('power_state', self.plug.is_on)
+        self.publish_val('sys_info', json.dumps(sysinfo))
+        self.publish_val('rssi', sysinfo['rssi'])
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import time
 import os
 import sys
 import yaml
+import logging
+import logging.config
 
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -16,8 +18,10 @@ mqtt_client = None
 config = {}
 devices = {}
 
+log = logging.getLogger("main")
+
 def on_connect(mqtt_client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    log.info(f"Connected with result code: {str(rc)}")
 
     if rc == 0:
         for device_id in devices:
@@ -26,11 +30,21 @@ def on_connect(mqtt_client, userdata, flags, rc):
         sys.exit(-1)
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-    for device_id in devices:
-        if msg.topic.startswith(device_id + '/'):
-            devices[device_id].on_message(msg.payload.decode('utf-8'))
+    log.debug(f"{msg.topic}: {str(msg.payload)}")
 
+    ss = msg.topic.split('/', 3)
+    if len(ss) == 3:
+        if ss[1] == 'command':
+            re_device_id = ss[0]
+            re_cmd = ss[2]
+            re_msg = msg.payload.decode('utf-8').strip()
+            log.debug(f"{re_device_id}: Executing command {re_cmd}: {re_msg}")
+            for device_id in devices:
+                if device_id == re_device_id:
+                    devices[device_id].on_command(re_cmd, re_msg)
+            return
+
+    log.warn(f"invalid topic: {msg.topic}")
 
 def on_sched():
     global devices
@@ -40,14 +54,18 @@ def on_sched():
 
 def parse_config():
     global config
-    print("parsing configuration...")
+    log.info("parsing configuration...")
 
     with open('config.yaml') as file:
         config = yaml.load(file, Loader=yaml.FullLoader)
+
+    log.info("configuration read!")
     
 def main():
     global devices
     global mqtt_client
+
+    logging.config.fileConfig(fname='logging.ini', disable_existing_loggers=False)
 
     parse_config()
     mqtt_client = mqtt.Client()
